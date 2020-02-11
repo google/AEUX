e9a8a69777de8474b3d5277866c126252b9b3c9a
Adam Plouff <adamplouff@gmail.com>
2020-02-11 15:40:25 -0500

    Brutalism cleanup

--- Ae/AEUX/src/App.vue
+++ Ae/AEUX/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <menus />
     <Panel>
         <AEUX />
     </Panel>
@@ -13,11 +12,6 @@
 
 // Create your own components and import them here
 import AEUX from "./components/AEUX.vue";
-import menus from "./components/menus.vue";
-
-// Dynamic CSS variables that automatically handle all app themes and changes:
-// https://github.com/Inventsable/starlette
-import starlette from "starlette";
 
 // Dynamic identification object that reports all panel and host information:
 // https://github.com/Inventsable/CEP-Spy
@@ -27,114 +21,16 @@ export default {
   name: "app",
   components: {
     AEUX,
-    menus
   },
   data: () => ({
-    csInterface: null,
-    vulcan: null
+    
   }),
-  created() {
-    this.csInterface = new CSInterface();
-    this.vulcan = new Vulcan();
-  },
-  mounted() {
-    // console.clear();
-    starlette.init();
-    this.csInterface = new CSInterface();
-    this.csInterface.addEventListener("console", this.consoler);
-
-    // Utility components already mounted prior to this
-    console.log(
-      `${spy.extName} ${spy.extVersion} : ${spy.isDev ? "DEV" : "BUILD"}`
-    );
-
-    this.loadUniversalScripts();
-  },
   methods: {
-    dispatchEvent(name, data) {
-      var event = new CSEvent(name, "APPLICATION");
-      event.data = data;
-      this.csInterface.dispatchEvent(event);
-    },
-    loadScript(path) {
-      this.csInterface.evalScript(`$.evalFile('${path}')`);
-    },
-    loadUniversalScripts() {
-      // Preloads any script located inside ./src/host/universal
-      let utilFolder = window.cep.fs.readdir(
-        `${spy.path.root}/src/host/universal/`
-      );
-      if (!utilFolder.err) {
-        let valids = utilFolder.data.filter(file => {
-          return /\.(jsx|jsfl)$/.test(file);
-        });
-        valids.forEach(file => {
-          this.loadScript(`${spy.path.root}/src/host/universal/${file}`);
-        });
-      }
-      // Preloads any script located in ./src/host/[appName]/
-      let hostFolder = window.cep.fs.readdir(
-        `${spy.path.root}/src/host/${spy.appName}/`
-      );
-      if (!hostFolder.err) {
-        let valids = hostFolder.data.filter(file => {
-          return /\.(jsx|jsfl)$/.test(file);
-        });
-        valids.forEach(file => {
-          this.loadScript(`${spy.path.root}/src/host/${spy.appName}/${file}`);
-        });
-      } else {
-        console.log(
-          `${spy.path.root}/src/host/${spy.appName} has no valid files or does not exist`
-        );
-      }
-    },
+
   }
 };
 </script>
 
 <style>
-/* Various helper styles to match application theme */
-@import url("https://fonts.googleapis.com/css?family=Open+Sans&display=swap");
-:root {
-  --quad: cubic-bezier(0.48, 0.04, 0.52, 0.96);
-  --quart: cubic-bezier(0.76, 0, 0.24, 1);
-  --quint: cubic-bezier(0.84, 0, 0.16, 1);
 
-  background-color: var(--color-bg);
-  color: var(--color-default);
-  font-family: "Open Sans", sans-serif;
-  font-size: 10px;
-}
-
-#app::-webkit-scrollbar {
-  display: block;
-}
-body::-webkit-scrollbar {
-  width: 0px;
-}
-
-::-webkit-scrollbar {
-  background-color: var(--color-scrollbar);
-  width: var(--width-scrollbar-track);
-}
-::-webkit-scrollbar-thumb {
-  width: var(--width-scrollbar-track);
-  background: var(--color-scrollbar-thumb);
-  border-radius: var(--radius-scrollbar-thumb);
-}
-::-webkit-scrollbar-thumb:hover {
-  background: var(--color-scrollbar-thumb-hover);
-}
-::-webkit-scrollbar-resizer {
-  display: none;
-  width: 0px;
-  background-color: transparent;
-}
-::-webkit-scrollbar-button {
-  height: 0px;
-}
-::-webkit-scrollbar-corner {
-  display: none;
-}
 </style>

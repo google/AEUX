de64a7c0e37ecbfd75c5e4463aec95ab435e29b6
Adam Plouff <adamplouff@gmail.com>
2020-02-28 10:11:38 -0500

    Brutalism menus

--- Ae/AEUX/src/App.vue
+++ Ae/AEUX/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <menus />
+    <Menus />
     <Panel>
       <AEUX />
     </Panel>
@@ -13,82 +13,82 @@
 
 // Create your own components and import them here
 import AEUX from "./components/AEUX.vue";
-import menus from "./components/menus.vue";
+// import menus from "./components/menus.vue";
 
 // Dynamic CSS variables that automatically handle all app themes and changes:
 // https://github.com/Inventsable/starlette
-import starlette from "starlette";
+// import starlette from "starlette";
 
 // Dynamic identification object that reports all panel and host information:
 // https://github.com/Inventsable/CEP-Spy
-import spy from "cep-spy";
+// import spy from "cep-spy";
 
 export default {
   name: "app",
   components: {
     AEUX,
-    menus
+    // menus
   },
   data: () => ({
-    csInterface: null,
-    vulcan: null
+    // csInterface: null,
+    // vulcan: null
   }),
   created() {
-    this.csInterface = new CSInterface();
-    this.vulcan = new Vulcan();
+    // this.csInterface = new CSInterface();
+    // this.vulcan = new Vulcan();
   },
   mounted() {
     // console.clear();
-    starlette.init();
-    this.csInterface = new CSInterface();
-    this.csInterface.addEventListener("console", this.consoler);
+    // starlette.init();
+    // this.csInterface = new CSInterface();
+    // this.csInterface.addEventListener("console", this.consoler);
 
-    // Utility components already mounted prior to this
-    console.log(
-      `${spy.extName} ${spy.extVersion} : ${spy.isDev ? "DEV" : "BUILD"}`
-    );
+    // // Utility components already mounted prior to this
+    // console.log(
+    //   `${spy.extName} ${spy.extVersion} : ${spy.isDev ? "DEV" : "BUILD"}`
+    // );
 
-    this.loadUniversalScripts();
+    // this.loadUniversalScripts();
   },
   methods: {
-    dispatchEvent(name, data) {
-      var event = new CSEvent(name, "APPLICATION");
-      event.data = data;
-      this.csInterface.dispatchEvent(event);
-    },
-    loadScript(path) {
-      this.csInterface.evalScript(`$.evalFile('${path}')`);
-    },
-    loadUniversalScripts() {
-      // Preloads any script located inside ./src/host/universal
-      let utilFolder = window.cep.fs.readdir(
-        `${spy.path.root}/src/host/universal/`
-      );
-      if (!utilFolder.err) {
-        let valids = utilFolder.data.filter(file => {
-          return /\.(jsx|jsfl)$/.test(file);
-        });
-        valids.forEach(file => {
-          this.loadScript(`${spy.path.root}/src/host/universal/${file}`);
-        });
-      }
-      // Preloads any script located in ./src/host/[appName]/
-      let hostFolder = window.cep.fs.readdir(
-        `${spy.path.root}/src/host/${spy.appName}/`
-      );
-      if (!hostFolder.err) {
-        let valids = hostFolder.data.filter(file => {
-          return /\.(jsx|jsfl)$/.test(file);
-        });
-        valids.forEach(file => {
-          this.loadScript(`${spy.path.root}/src/host/${spy.appName}/${file}`);
-        });
-      } else {
-        console.log(
-          `${spy.path.root}/src/host/${spy.appName} has no valid files or does not exist`
-        );
-      }
-    },
+    // dispatchEvent(name, data) {
+    //   var event = new CSEvent(name, "APPLICATION");
+    //   event.data = data;
+    //   this.csInterface.dispatchEvent(event);
+    // },
+    // loadScript(path) {
+    //   this.csInterface.evalScript(`$.evalFile('${path}')`);
+    // },
+    // loadUniversalScripts() {
+    //   // Preloads any script located inside ./src/host/universal
+    //   let utilFolder = window.cep.fs.readdir(
+    //     `${spy.path.root}/src/host/universal/`
+    //   );
+    //   if (!utilFolder.err) {
+    //     let valids = utilFolder.data.filter(file => {
+    //       return /\.(jsx|jsfl)$/.test(file);
+    //     });
+    //     valids.forEach(file => {
+    //       this.loadScript(`${spy.path.root}/src/host/universal/${file}`);
+    //     });
+    //   }
+    //   // Preloads any script located in ./src/host/[appName]/
+    //   let hostFolder = window.cep.fs.readdir(
+    //     `${spy.path.root}/src/host/${spy.appName}/`
+    //   );
+    //   if (!hostFolder.err) {
+    //     let valids = hostFolder.data.filter(file => {
+    //       return /\.(jsx|jsfl)$/.test(file);
+    //     });
+    //     valids.forEach(file => {
+    //       this.loadScript(`${spy.path.root}/src/host/${spy.appName}/${file}`);
+    //     });
+    //   } else {
+    //     console.log(
+    //       `${spy.path.root}/src/host/${spy.appName} has no valid files or does not exist`
+    //     );
+    //   }
+    // },
   }
 };
 </script>

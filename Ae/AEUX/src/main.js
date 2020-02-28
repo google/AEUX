39d639cadec974d120810bdd7049f15a107b106e
Adam Plouff <adamplouff@gmail.com>
2020-02-28 10:11:48 -0500

    Cleanup

--- Ae/AEUX/src/main.js
+++ Ae/AEUX/src/main.js
@@ -6,11 +6,12 @@ Vue.config.devtools = false;
 
 // Brutalism library support
 import {
-	ButtonGroup, Button, Col, Divider, Dropdown, Dropzone, FileInput, Fold, Icon, InputNumber, InputScroll, Menus, PanelInfo, Panel, Row, Tabs, Toggle, Wrapper, Altpan, Autofocus, Pan
+	Alert, ButtonGroup, Button, Col, Divider, Dropdown, Dropzone, FileInput, Fold, Footer, Icon, InputNumber, InputScroll, Input, Menus, PanelInfo, Panel, RotationControl, Row, Tabs, TextArea, Toggle, Wrapper, Altpan, Autofocus, Pan
 } from "../../../../../Modules/brutalism/src/brutalism";
 
 require("../node_modules/@mdi/font/css/materialdesignicons.css");
 
+Vue.component("Alert", Alert);
 Vue.component("Button-Group", ButtonGroup);
 Vue.component("Button", Button);
 Vue.component("Col", Col);
@@ -19,14 +20,18 @@ Vue.component("Dropdown", Dropdown);
 Vue.component("Dropzone", Dropzone);
 Vue.component("File-Input", FileInput);
 Vue.component("Fold", Fold);
+Vue.component("Footer", Footer);
 Vue.component("Icon", Icon);
 Vue.component("Input-Number", InputNumber);
 Vue.component("Input-Scroll", InputScroll);
+Vue.component("Input", Input);
 Vue.component("Menus", Menus);
 Vue.component("Panel-Info", PanelInfo);
 Vue.component("Panel", Panel);
+Vue.component("Rotation-Control", RotationControl);
 Vue.component("Row", Row);
 Vue.component("Tabs", Tabs);
+Vue.component("TextArea", TextArea);
 Vue.component("Toggle", Toggle);
 Vue.component("Wrapper", Wrapper);
 Vue.directive("altpan", Altpan);

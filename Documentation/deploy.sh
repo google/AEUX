3b8a8008fd0bac820227cb318aba085f50556e0a
Adam Plouff <adamplouff@gmail.com>
2021-08-26 16:26:34 -0400

    Rename Master to Main

--- Documentation/deploy.sh
+++ Documentation/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/google/sketch2ae.git master:gh-pages
+git push -f https://github.com/google/sketch2ae.git main:gh-pages
 
 cd -
